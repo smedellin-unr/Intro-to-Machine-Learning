6ec3fdb11127ec6a5ae38133d367a9735b3d426b
smedellin-unr <smedellin@nevada.unr.edu>
2021-09-20 21:03:38 -0700

    the best feature splitting works, but not sure about anything else

--- Project 1/WorkSpace/decision_tree.py
+++ Project 1/WorkSpace/decision_tree.py
@@ -2,6 +2,7 @@ import numpy as np
 from math import log
 from scipy import spatial
 from random import randint
+from enum import Enum
 
 # TODO: how to read in a text file in python
 # Have it output features and labels in correct numpy format
@@ -26,14 +27,31 @@ def read_features_labels(filepath:str=None) -> tuple:
 
     return (X,Y)
 
+class Leaf:
+    def __init__(
+        self,
+        decision:bool,
+        side: bool,
+    ):
+        self.decision = decision
+        self.side = side
+
+    def __str__(self):
+        if self.side:
+            return 'Right Leaf: Yes' if self.decision else 'Right Leaf: No'
+        else:
+            return 'Left Leaf: Yes' if self.decision else 'Left Leaf: No'
 
 class Node:
     def __init__(
         self,
         X: np.ndarray, 
         Y: np.array, 
-        max_depth=None,
-        depth=None,
+        max_depth: int=None,
+        depth: int=None,
+        node_type: str=None,
+        leaf_side: bool=None,
+        leaf_decision: bool=None,
     ) :
         self.Y = Y # 1D array
         self.X = X # 2D array
@@ -41,16 +59,24 @@ class Node:
         self.n_samples = self.X.shape[0]
         self.n_features = self.X.shape[1]
 
-        self.max_depth = max_depth if max_depth else self.n_features # TODO: make sure this is legit
+        self.xy_matrix = np.column_stack((X,Y)) # features and labels concatenated into one 2D matrix
+
+        self.node_type = node_type if node_type else 'root'
+
+        self.max_depth = max_depth if max_depth else self.n_features
         self.depth = depth if depth else 0
 
         # TODO: Entropy at the node level
         self.entropy = Node.get_entropy(self.Y)
 
-        self.left = None
+        # Can be decision node of leaf
+        self.left = None 
         self.right = None
 
-        self.best_feature = None
+        self.best_feature = 0
+        self.best_information_gain = 0.0
+
+        ## execute function that decides
 
     @staticmethod
     def get_entropy(Y:np.array) -> np.float16:
@@ -97,47 +123,139 @@ class Node:
         Determine the feature yields
         the most information gain
         '''
-        #true_rows, false_rows = Node.get_partitions(self.X, self.Y)
-        xy_matrix = np.column_stack((self.X,self.Y))
-        best_feature = None
-        best_value = None
         best_information_gain = 0.0
+        best_feature = None
+        # leaf decision assignment
+        best_right_leaf_estimate = None
+        best_left_leaf_estimate = None
+        leaf_side = None
+        leaf_decision = None
+        # used for assessing where split will occur
+        best_right_entropy = 0.0
+        best_left_entropy = 0.0
 
+        print(self.xy_matrix)
+        print('\n')
         for column in range(self.n_features):
             left_entropy = right_entropy = left_feature_size = right_feature_size = total_size = 0
+            right_leaf_estimate = left_leaf_estimate = None
             # calculate entropy for left branch
-            temp_label = xy_matrix[xy_matrix[:,column] == False][:, -1]
-            if temp_label.size == 0:
+            left_label = self.xy_matrix[self.xy_matrix[:,column] == False][:, -1]
+            if left_label.size == 0:
                 # No 'NO' labels
                 pass
             else:
-                left_feature_size = temp_label.shape[0]
-                left_entropy = Node.get_entropy(temp_label)
+                left_feature_size = left_label.shape[0]
+                left_entropy = Node.get_entropy(left_label)
+                left_leaf_estimate = True if np.sum(left_label) / left_label.shape[0] > 0.5 else False
+                #print("left entropy: ",left_entropy)
+                #print("shape: ",temp_label.shape[0])
+                #print(temp_label)
+                #print("the estimate: ",left_leaf_estimate)
 
             # calculate entropy for right branch
-            temp_label = xy_matrix[xy_matrix[:,column] == True][:, -1]
-            if temp_label.size == 0:
+            right_label = self.xy_matrix[self.xy_matrix[:,column] == True][:, -1]
+            if right_label.size == 0:
                 # No 'YES' labels
                 pass
             else:
-                right_feature_size = temp_label.shape[0]
-                right_entropy = Node.get_entropy(temp_label)
+                right_feature_size = right_label.shape[0]
+                right_entropy = Node.get_entropy(right_label)
+                right_leaf_estimate = True if np.sum(right_label) / right_label.shape[0] > 0.5 else False
+                #print("right entropy: ",right_entropy)
 
             # calculate Information Gain for split
             total_size = left_feature_size + right_feature_size
             information_gain = self.entropy - ((left_feature_size / total_size) * left_entropy + (right_feature_size / total_size) * right_entropy)
+            '''
+            print("feature {} and IG {}".format(column + 1, information_gain))
+            print("Left Entropy: ", left_entropy)
+            print("left proportion: ", left_feature_size / total_size)
+            print("Right Entropy: ", right_entropy)
+            print("right proportion: ", right_feature_size / total_size)
+            print("Information gain: ",information_gain)
+            '''
 
             if information_gain >= best_information_gain:
                 best_feature, best_information_gain = column, information_gain
+                best_right_leaf_estimate = right_leaf_estimate
+                best_right_entropy = right_entropy
+                best_left_leaf_estimate = left_leaf_estimate
+                best_left_entropy = left_entropy
                 
-        return (best_feature, best_information_gain)
+        if best_right_entropy >= best_left_entropy:
+            leaf_side = True
+            leaf_decision = best_right_leaf_estimate
+        else:
+            leaf_side = False                   # TODO: make this prettier
+            leaf_decision = best_left_leaf_estimate
+
+        return (best_feature, best_information_gain, leaf_side, leaf_decision)
+
+    def leaf_assignment(self,leaf_side: bool, leaf_assignment: bool):
+        pass
+
+    def grow_tree(self):
+        '''
+        Recursive function
+        to build decision tree
+        '''
+
+        best_feature, best_information_gain, leaf_side, leaf_decision = self.best_split()
+
+        if (best_feature is not None) and (best_information_gain != 0):
+            self.best_feature = best_feature
+            self.best_information_gain = best_information_gain
+            print("best feature:", best_feature)
+
+            # if leaf is on the right decision branch...
+            if not leaf_side:
+                # make the right decision branch a leaf
+                self.right = Leaf(leaf_decision, leaf_side)
+
+                # make the left decision branch a node
+                left_side_dataset = np.copy(self.xy_matrix[self.xy_matrix[:,best_feature] == False])
+                left = Node(
+                    X=left_side_dataset[:,:-1],
+                    Y=left_side_dataset[:,-1],
+                    depth=self.depth+1,
+                    max_depth=self.max_depth,
+                    node_type='left_node'
+                )
+                
+                self.left = left
+
+                print(self.right)
+                print(self.left.node_type)
+
+                self.left.grow_tree()
+                
+            # if leaf is on the left decision branch
+            else:
+                self.left = Leaf(leaf_decision, leaf_side)
+                right_side_dataset = np.copy(self.xy_matrix[self.xy_matrix[:,best_feature] == True])
+                right = Node(
+                    X=right_side_dataset[:,:-1],
+                    Y=right_side_dataset[:,-1],
+                    depth=self.depth+1,
+                    max_depth=self.max_depth,
+                    node_type='right_node'
+                )
+
+                self.right = right
+                print(self.left)
+                print(self.right.node_type)
+                self.right.grow_tree()
         
 
 if __name__ == "__main__":
 
-    X, Y = read_features_labels('data_1.txt')
+    X, Y = read_features_labels('data_set_TV.txt')
     n_=Node(X=X, Y=Y)
     best_feature = best_information_gain = 0
-    best_feature, best_information_gain = n_.best_split()
-    print(best_feature, best_information_gain)
+    print('root node')
+    n_.grow_tree()
+    #print(best_feature, best_information_gain)
+    #print(n_.best_feature, n_.best_information_gain)
+    #print(best_feature, best_information_gain)
     t, f = Node.get_partitions(X,Y)
